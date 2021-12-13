71446d1e9ca97fcff8809173988a427d7c36a4d3
minlebay <81867441+minlebay@users.noreply.github.com>
2021-12-13 14:53:48 +0300

    Update SecurityConfig.java

--- src/main/java/com/JM/BootCRUD/config/SecurityConfig.java
+++ src/main/java/com/JM/BootCRUD/config/SecurityConfig.java
@@ -52,43 +52,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.formLogin()
-                // указываем страницу с формой логина
-                //.loginPage("/login")
-                //указываем логику обработки при логине
                 .successHandler(new LoginSuccessHandler())
-                // указываем action с формы логина
                 .loginProcessingUrl("/login")
-                // обработчик неудачного логина
                 .failureHandler(new LoginFailureHandler())
-                // Указываем параметры логина и пароля с формы логина
                 .usernameParameter("username")
                 .passwordParameter("password")
-                // даем доступ к форме логина всем
                 .permitAll();
-
         http.logout()
-                // разрешаем делать логаут всем
                 .permitAll()
-                // указываем URL при удачном логауте
                 .logoutSuccessUrl("/login")
-                // указываем URL логаута
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                //выклчаем кроссдоменную секьюрность (на этапе обучения неважна)
                 .and().csrf().disable();
-
-        http
-                // делаем страницу регистрации недоступной для авторизированных пользователей
-                .authorizeRequests()
-                //страницы аутентификации доступна всем
+        http.authorizeRequests()
                 .antMatchers("/login").anonymous()
-                //страница пользователя доступна только ролям пользователь и админ.
                 .antMatchers("/user/**").hasAnyRole("USER", "ADMIN")
-                //страницы и подстраницы доступные только админам
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest()
-
                 .authenticated();
-
         http.httpBasic();
     }
 }
